b1d1a52e88753dee3846b620d0330b6ac121c9a9
lqs66 <liuqiaosen666@163.com>
2024-11-25 11:27:47 +0800

    add funcID MD

--- llvm/lib/Transforms/PreModelChecking/AddMetaData.cpp
+++ llvm/lib/Transforms/PreModelChecking/AddMetaData.cpp
@@ -1,12 +1,17 @@
 #include "PreModelChecking.h"
 llvm::PreservedAnalyses PreModelChecking::AddMetaData::run(llvm::Module &M, llvm::ModuleAnalysisManager &MAM) {
+    llvm::outs() << "Add MetaData funcID for all functions...\n";
     llvm::outs() << "Add MetaData inCallID for indirect callsites...\n";
     runOnModule(M);
     return llvm::PreservedAnalyses::all();
 }
 void PreModelChecking::AddMetaData::runOnModule(llvm::Module &M){
     int inCallID = 0;
+    int funcID = 0;
     for(auto& F : M){
+        llvm::Constant* funcIDConstant = llvm::ConstantInt::get(llvm::Type::getInt32Ty(M.getContext()), funcID++);
+        auto *fidMDNode = llvm::MDNode::get(M.getContext(), llvm::ConstantAsMetadata::get(funcIDConstant));
+        F.setMetadata("funcID", fidMDNode);
         if(F.isDeclaration()){
             continue;
         }
