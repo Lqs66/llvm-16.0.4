22f894de89ad4a88314bc8e0759e66594691a8e0
lqs66 <liuqiaosen666@163.com>
2025-01-24 15:05:09 +0800

    tmp

--- llvm/lib/Transforms/PreModelChecking/PreModelChecking.h
+++ llvm/lib/Transforms/PreModelChecking/PreModelChecking.h
@@ -101,13 +101,5 @@ namespace PreModelChecking{
         static bool isRequired() { return true; }
         void runOnModule(llvm::Module &M);
     }; 
-    
-    class CreateFuncSections : public llvm::PassInfoMixin<CreateFuncSections>{
-    public:
-        llvm::PreservedAnalyses run(llvm::Module &M, llvm::ModuleAnalysisManager &MAM);
-        
-        static bool isRequired() { return true; }
-        void runOnModule(llvm::Module &M);
-    };
 }
 #endif //PREMODELCHECKING_H
