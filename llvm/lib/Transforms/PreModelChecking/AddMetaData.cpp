f4627100a41243f39192f0b4d127c623b5dcf646
Lqs66 <liuqiaosen666@163.com>
2025-02-25 19:22:28 +0800

    add heapAllocID metadata

--- llvm/lib/Transforms/PreModelChecking/AddMetaData.cpp
+++ llvm/lib/Transforms/PreModelChecking/AddMetaData.cpp
@@ -2,12 +2,14 @@
 llvm::PreservedAnalyses PreModelChecking::AddMetaData::run(llvm::Module &M, llvm::ModuleAnalysisManager &MAM) {
     llvm::outs() << "Add MetaData funcID for all functions...\n";
     llvm::outs() << "Add MetaData inCallID for indirect callsites...\n";
+    llvm::outs() << "Add MetaData heapAllocID for heap alloc instructions...\n";
     runOnModule(M);
     return llvm::PreservedAnalyses::all();
 }
 void PreModelChecking::AddMetaData::runOnModule(llvm::Module &M){
     int inCallID = 0;
     int funcID = 0;
+    int heapAllocID = 0;
     for(auto& F : M){
         llvm::Constant* funcIDConstant = llvm::ConstantInt::get(llvm::Type::getInt32Ty(M.getContext()), funcID++);
         auto *fidMDNode = llvm::MDNode::get(M.getContext(), llvm::ConstantAsMetadata::get(funcIDConstant));
@@ -23,6 +25,17 @@ void PreModelChecking::AddMetaData::runOnModule(llvm::Module &M){
                         llvm::Constant* inCallIDConstant = llvm::ConstantInt::get(llvm::Type::getInt32Ty(M.getContext()), inCallID++);
                         auto *metadataNode = llvm::MDNode::get(M.getContext(), llvm::ConstantAsMetadata::get(inCallIDConstant));
                         callInst->setMetadata("inCallID", metadataNode);
+                    } else if (auto *callee = callInst->getCalledFunction()) {
+                        auto calleeName = callee->getName().str();
+                        if (calleeName == "malloc" || 
+                            calleeName == "calloc" || 
+                            calleeName == "realloc" || 
+                            calleeName == "_Znwm" || 
+                            calleeName == "_Znam") {
+                            llvm::Constant* heapAllocIDConstant = llvm::ConstantInt::get(llvm::Type::getInt32Ty(M.getContext()), heapAllocID++);
+                            auto *metadataNode = llvm::MDNode::get(M.getContext(), llvm::ConstantAsMetadata::get(heapAllocIDConstant));
+                            callInst->setMetadata("heapAllocID", metadataNode);
+                        }
                     }
                 }
             }
